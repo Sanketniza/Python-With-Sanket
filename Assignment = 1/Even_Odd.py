b20b895900bbf24cb98351d4a89816369108c188
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-08-20 23:22:04 +0530

    first commit

---
+++ Assignment = 1/Even_Odd.py
@@ -0,0 +1,7 @@
+# Write a program to check whether a number is even or odd.
+
+a = int(input("Enter the number:- "))
+if a%2==0:
+    print("Even")
+else:
+    print("odd")
---
+++ Assignment = 1/Vowel.py
@@ -0,0 +1,7 @@
+# Write a program to check whether a character is vowel or not.
+
+ch=input("Enter the character:- ")
+if ch=='a'or ch=='A'or ch=='e'or ch=='E'or ch=='i'or ch=='I'or ch=='o'or ch=='O'or ch=='u'or ch=='U':
+    print("vowel")
+else:
+    print("not vowels")
