9375327f223434f90ffc2fadd26b97c9a1de0c98
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-08-20 23:41:37 +0530

    Adding list function in 3rd Assignment.

---
+++ Assignment = 3/List.py
@@ -0,0 +1,73 @@
+#  Performed all the operations on list
+#  1. append
+#  2. clear
+#  3. copy
+#  4. count
+#  5. extend
+#  6. index
+#  7. insert
+#  8. pop
+#  9. remove
+#  10. reverse
+#  11. sort
+
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# append
+l.append(11)
+print(l)
+print('\n')
+
+# copy
+l.copy()
+print(l)
+print('\n')
+
+# count
+l.count(1)
+print(l)
+print('\n')
+
+# extend
+l.extend([12, 13, 14])
+print(l)
+print('\n')
+
+# index
+l.index(1)
+print(l)
+print('\n')
+
+# insert
+l.insert(0, 0)
+print(l)
+print('\n')
+
+# pop
+l.pop(0)
+print(l)
+print('\n')
+
+# remove
+l.remove(1)
+print(l)
+print('\n')
+
+# reverse
+l.reverse()
+print(l)
+print('\n')
+
+# sort
+l.sort()
+print(l)
+print('\n')
+
+
+# clear
+l.clear()
+print(l)
+print('\n')
+
+
